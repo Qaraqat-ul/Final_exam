a7832c9b44dd3ade42e353117b009f14e189be58
karakatzumagali <karakat.zhumagali@narxoz.kz>
2025-12-18 19:04:12 +0500

    Add CustomUserDetailsService

---
+++ src/main/java/com/example/Final_exam/security/CustomUserDetailsService.java
@@ -0,0 +1,32 @@
+package com.example.Final_exam.security;
+
+import com.example.Final_exam.entity.User;
+import com.example.Final_exam.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.*;
+import org.springframework.stereotype.Service;
+
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found: " + username));
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getUsername(),
+                user.getPassword(),
+                user.getRoles().stream()
+                        .map(r -> new SimpleGrantedAuthority(r.getName()))
+                        .collect(Collectors.toSet())
+        );
+    }
+}
+
